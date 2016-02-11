60553c16602e433387369dc3dc7d4e9c0bd89aed
Huludini <tom8004@hotmail.com>
2016-02-10 23:00:05 -0500

    Added the remaining files that were missed from the voting feature.

---
+++ resources/lang/en/vote.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Vote Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'button' => 'Vote on :name',
+    'double_rewards' => 'Double Rewards',
+    'cooldown' => 'You can vote again in...',
+    'cooldown_done' => 'You can vote again!',
+    'no_sites' => 'There aren\'t any sites to vote for, please contact an administrator.',
+    'already_voted' => 'Sorry, but you\'ve already voted for this site.',
+    'time' => [
+        'hours' => 'hours',
+        'minutes' => 'minutes',
+        'seconds' => 'seconds'
+    ],
+
+    'success' => [
+        'title' => 'Success',
+        'continue' => 'Click below to continue!',
+        'notice' => 'Enter the code on the next page accurately or you will <b>NOT</b> get your reward!',
+        'button' => 'Continue'
+    ],
+
+    'add' => 'Add Site',
+    'view' => 'View Sites',
+
+    'fields' => [
+        'double_rewards' => 'Double Rewards',
+        'name' => 'Name',
+        'link' => 'External Link',
+        'type' => [
+            'title' => 'Reward Type',
+            'virtual' => 'Virtual',
+            'cubi' => 'Cubi'
+        ],
+        'reward' => 'Reward',
+        'limit' => 'Time Limit'
+    ],
+    'create' => 'Create New Site',
+    'index' => 'Viewing Sites',
+    'edit' => 'Edit Site: :name',
+    'add_button' => 'Create Site',
+    'update_button' => 'Update Site',
+    'create_success' => 'Your site has been created!',
+    'edit_success' => 'Your changes have been saved!',
+
+];
