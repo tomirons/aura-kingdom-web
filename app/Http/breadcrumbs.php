501db44b9e19fbb045398b6fd73152826643106f
Huludini <tom8004@hotmail.com>
2016-02-16 16:12:34 -0500

    Added all the breadcrumbs for the admin side so far.

--- app/Http/breadcrumbs.php
+++ app/Http/breadcrumbs.php
@@ -38,3 +38,106 @@ Breadcrumbs::register( 'ranking.index', function( $breadcrumbs )
     $breadcrumbs->parent( 'home' );
     $breadcrumbs->push( trans( 'main.apps.ranking' ) );
 });
+
+/*
+|--------------------------------------------------------------------------
+| Admin Breadcrumbs
+|--------------------------------------------------------------------------
+*/
+Breadcrumbs::register( 'admin.index', function( $breadcrumbs )
+{
+    $breadcrumbs->push( trans( 'main.dashboard' ), route( 'admin.index' ) );
+});
+
+/* System */
+Breadcrumbs::register( 'admin.system.settings', function( $breadcrumbs )
+{
+    $breadcrumbs->parent( 'admin.index' );
+    $breadcrumbs->push( trans( 'main.settings' ) );
+});
+
+Breadcrumbs::register( 'admin.system.apps', function( $breadcrumbs )
+{
+    $breadcrumbs->parent( 'admin.index' );
+    $breadcrumbs->push( trans( 'system.apps' ) );
+});
+
+/* Members */
+Breadcrumbs::register( 'admin.members.manage', function( $breadcrumbs )
+{
+    $breadcrumbs->parent( 'admin.index' );
+    $breadcrumbs->push( trans( 'main.apps.members' ) );
+    $breadcrumbs->push( trans( 'members.manage' ) );
+});
+
+/* News */
+Breadcrumbs::register( 'admin.news', function( $breadcrumbs )
+{
+    $breadcrumbs->parent( 'admin.index' );
+    $breadcrumbs->push( trans( 'main.apps.news' ) );
+});
+
+Breadcrumbs::register( 'admin.news.index', function( $breadcrumbs )
+{
+    $breadcrumbs->parent( 'admin.news' );
+    $breadcrumbs->push( trans( 'news.index' ) );
+});
+
+Breadcrumbs::register( 'admin.news.create', function( $breadcrumbs )
+{
+    $breadcrumbs->parent( 'admin.news' );
+    $breadcrumbs->push( trans( 'news.create' ) );
+});
+
+Breadcrumbs::register( 'admin.news.edit', function( $breadcrumbs, \App\Article $article )
+{
+    $breadcrumbs->parent( 'admin.news' );
+    $breadcrumbs->push( trans( 'news.edit', ['title' => $article->title] ) );
+});
+
+Breadcrumbs::register( 'admin.news.settings', function( $breadcrumbs )
+{
+    $breadcrumbs->parent( 'admin.news' );
+    $breadcrumbs->push( trans( 'main.settings' ) );
+});
+
+/* Donate */
+Breadcrumbs::register( 'admin.donate.settings', function( $breadcrumbs )
+{
+    $breadcrumbs->parent( 'admin.index' );
+    $breadcrumbs->push( trans( 'main.apps.donate' ) );
+    $breadcrumbs->push( trans( 'main.settings' ) );
+});
+
+/* Vote */
+Breadcrumbs::register( 'admin.vote', function( $breadcrumbs )
+{
+    $breadcrumbs->parent( 'admin.index' );
+    $breadcrumbs->push( trans( 'main.apps.vote' ) );
+});
+
+Breadcrumbs::register( 'admin.vote.index', function( $breadcrumbs )
+{
+    $breadcrumbs->parent( 'admin.vote' );
+    $breadcrumbs->push( trans( 'vote.index' ) );
+});
+
+Breadcrumbs::register( 'admin.vote.create', function( $breadcrumbs )
+{
+    $breadcrumbs->parent( 'admin.vote' );
+    $breadcrumbs->push( trans( 'vote.create' ) );
+});
+
+Breadcrumbs::register( 'admin.vote.edit', function( $breadcrumbs, \App\VoteSite $site )
+{
+    $breadcrumbs->parent( 'admin.vote' );
+    $breadcrumbs->push( trans( 'vote.edit', ['name' => $site->name] ) );
+});
+
+/* Ranking */
+Breadcrumbs::register( 'admin.ranking.settings', function( $breadcrumbs )
+{
+    $breadcrumbs->parent( 'admin.index' );
+    $breadcrumbs->push( trans( 'main.apps.ranking' ) );
+    $breadcrumbs->push( trans( 'main.settings' ) );
+});
